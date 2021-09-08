6e9141b0fac5168e58a90ca0e719496adf568226
allenrahbar <allen.rahbar@gmail.com>
2021-09-08 13:26:16 +0000

    add: code for subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -81,7 +81,7 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
       
     }   else if (operator === "-") {
-        return[operand1 - operand2,"subtract"];
+        return[operand1 - operand2, "subtract"];
     }else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -118,9 +118,10 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubtractQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById('operator').textContent = "-";
+
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
